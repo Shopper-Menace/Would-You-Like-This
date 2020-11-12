822a7dddca377d50ad285248896d3171b71f31fd
Alex Dunne <alex12dunne@gmail.com>
2020-11-12 12:56:03 -0500

    cart update before lunch

--- client/components/cart/userCart.js
+++ client/components/cart/userCart.js
@@ -2,22 +2,20 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 const UserCart = props => {
-  console.log(props.cart, 'USER CART')
-
   return (
     <div>
       USER CART
-      {/* {!props.cart ? (
+      {!props.cart ? (
         <div>No Items</div>
       ) : (
-        props.cart.map((item) => <div>{item}</div>)
-      )} */}
+        props.cart.map(item => <div>{item}</div>)
+      )}
     </div>
   )
 }
 
 const mapState = state => ({
-  cart: state.cart
+  user: state.user
 })
 
 export default connect(mapState, null)(UserCart)
