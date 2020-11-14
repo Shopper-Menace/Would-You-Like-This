e4e1c5a554b966cb8a6fb3c3b1ab3c719a6708c5
Alex Dunne <alex12dunne@gmail.com>
2020-11-14 16:33:51 -0500

    add to local cart from all products is now fully functional, last thing for entire cart task is to add checkout and messing around with being able to update qty of particular item in cart

--- client/components/productsView.js
+++ client/components/productsView.js
@@ -1,10 +1,69 @@
-import React, {useState} from 'react'
-import axios from 'axios'
+import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
-import {addToCart, fetchSingleProduct} from '../store'
+import {addToCart, fetchSingleProduct, addToLocal} from '../store'
 
-const Products = ({products, user, handleClick, addItemToCart}) => {
+const Products = ({products, user, handleClick, addToLocal}) => {
+  if (!user.id) {
+    return (
+      <div className="viewallcontainer">
+        <div className="viewallsidebar">
+          <h1>WYLT Prime</h1>
+        </div>
+        <div className="productview">
+          {products.map(product => {
+            const {id, imageUrl, name, price, category, description} = product
+            let productAsArray = [
+              id,
+              category,
+              name,
+              price,
+              description,
+              imageUrl
+            ]
+
+            return (
+              <div key={id} className="prod">
+                <div className="prodbox">
+                  <Link to={`/products/${id}`}>
+                    <img className="prodimg" src={imageUrl} />
+                  </Link>
+                </div>
+                <div className="prodtext">
+                  <Link to={`/products/${id}`} onClick={() => handleClick(id)}>
+                    <h5 className="productname">{name}</h5>
+                  </Link>
+                  <div>{`$${price / 100}`}</div>
+                  <button
+                    onClick={() => {
+                      addToLocal(productAsArray)
+                    }}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+                <div className="adminButtons">
+                  <button className="edit" type="button">
+                    Edit
+                  </button>
+                  <button className="delete" type="button">
+                    Delete
+                  </button>
+                  <button className="setFeatured" type="button">
+                    Set Featured
+                  </button>
+                  <button className="setRecommended" type="button">
+                    Set Recommended
+                  </button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="viewallcontainer">
       <div className="viewallsidebar">
@@ -70,6 +129,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   addItemToCart: (orderId, itemId) => dispatch(addToCart(orderId, itemId)),
-  handleClick: id => dispatch(fetchSingleProduct(id))
+  handleClick: id => dispatch(fetchSingleProduct(id)),
+  addToLocal: item => dispatch(addToLocal(item))
 })
 export default connect(mapStateToProps, mapDispatchToProps)(Products)
